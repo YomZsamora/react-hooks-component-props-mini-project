09d7f167361d94111300358e682a28752f5fdfd0
yomZsamora <samora.y@adzumi.co.ke>
2022-03-27 12:23:29 +0300

    Done rendering about and image in About Component.

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function About() {
+function About({bio, image}) {
    return (
       <React.Fragment>
          <aside>
-            
+            {image ? <img src={image} alt="blog logo" /> : <img src="https://via.placeholder.com/215" alt="blog logo" />}
+            <p>{bio}</p>
          </aside>
-
       </React.Fragment>
    )
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
+import About from "./About";
 
 console.log(blogData);
 
 function App() {
   return (
      <React.Fragment>
-         <div className="App">
+         {/* <div className="App">
          You're on your own from here! Follow the deliverables; test things out in
          the browser as you write your code; and good luck!
-         </div>
+         </div> */}
          <Header blogName={blogData.name}/>
+         <About bio={blogData.about} image={blogData.image} />
      </React.Fragment>
   );
 }
