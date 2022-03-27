9583b010b7f05353176811bcf91aa8e6042a1408
yomZsamora <samora.y@adzumi.co.ke>
2022-03-27 12:14:40 +0300

    Created About Component.

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+   return (
+      <React.Fragment>
+         <aside>
+            
+         </aside>
+
+      </React.Fragment>
+   )
+}
+
+export default About;
